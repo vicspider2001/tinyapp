d450fd462ea2b9346042191490c27a5c8a6c93a9
Victor Anogwih <anogwihvictor@gmail.com>
2024-01-01 22:16:38 -0500

    Added urls_show

--- express_server.js
+++ express_server.js
@@ -31,6 +31,18 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
 
+ app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const templateVars = {id: req.params.id, longURL: urlDatabase[id]}
+  res.render("urls_show", templateVars);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
