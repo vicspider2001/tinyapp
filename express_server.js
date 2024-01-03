93e0c7c6f0ee9c4d9a32dc4d008357084c1e218b
Victor Anogwih <anogwihvictor@gmail.com>
2024-01-03 01:28:39 -0500

    update

--- express_server.js
+++ express_server.js
@@ -42,7 +42,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/u/:id", (req, res) => {
+app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const templateVars = {id: req.params.id, longURL: urlDatabase[id]};
   res.render("urls_show", templateVars);
@@ -58,7 +58,7 @@ app.post("/urls", (req, res) => {
   }
   const shortURLID = generateRandomString(6);
   urlDatabase[shortURLID] = longURL;
-  res.redirect(`/u/${shortURLID}`);
+  res.redirect(`/urls/${shortURLID}`);
 });
 
 
