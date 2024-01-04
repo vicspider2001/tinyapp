5c41151df1d71a4ddabde1a5f4bce87ce2c2aab4
Victor Anogwih <anogwihvictor@gmail.com>
2024-01-04 04:05:22 -0500

    merged features

--- express_server.js
+++ express_server.js
@@ -110,6 +110,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
