650040a0be87513a3f3faeffe9c70bea725c4100
Victor Anogwih <anogwihvictor@gmail.com>
2024-01-16 02:56:10 -0500

    Comment added, Major Functionalities added

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+// Required modules
 const express = require("express");
 const app = express();
 const cookieSession  = require('cookie-session')
@@ -5,7 +6,7 @@ const PORT = 8080; // default port 8080;
 const bcrypt = require("bcryptjs");
 const { generateRandomString, getUserByEmail, urlsForUser } = require("./helpers");
 
-
+// Set EJS as the view engine and use middleware
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession ({
@@ -14,7 +15,7 @@ app.use(cookieSession ({
   maxAge: 24 * 60 * 60 * 1000, // 24 hours
 }));
 
-
+//tinyapp sample database
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.lighthouselabs.ca",
@@ -26,6 +27,7 @@ const urlDatabase = {
   },
 };
 
+//tinyapp sample users
 const users = {
   OI9M: {
     id: 'OI9M',
@@ -39,15 +41,21 @@ const users = {
   }
 };
 
-
+// Root route
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const userDetails = users[req.session.user_id];
+  if(!userDetails) {
+    res.redirect("/login");
+  }
+  res.redirect("/urls");
 });
 
+//JSON representation of tinyapp's database
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// New URL page route
 app.get("/urls/new", (req, res) => {
   const userDetails = users[req.session.user_id];
   const templateVars = { 
@@ -60,32 +68,45 @@ app.get("/urls/new", (req, res) => {
   
 });
 
+// Individual URL details route
 app.get("/urls/:id", (req, res) => {
   const userDetails = users[req.session.user_id];
-  const userPages = urlsForUser(userDetails.id, urlDatabase);
+  const userPages = urlsForUser(userDetails.id, urlDatabase); //helper function used to get specific user URLs
   if(!userDetails || !userPages) {
-    return res.send(403).send("You must login to access your URLs!");
+    return res.status(403).render("error", { error: "You must log in to access your URLs!" });
   }
+
   const id = req.params.id;
   const userData = users[req.session.user_id];
-  const templateVars = {id: req.params.id, longURL: urlDatabase[id].longURL, userDetails: userData,};
+  let urlData = urlDatabase[id];
+  if(!urlData) {
+    return res.status(404).render("error", { error: "Short URL not found" });
+  }
+
+  if(urlData.userID !== userDetails.id) {
+    return res.status(403).render("error", { error: "You do not have permission to access this URL!" });
+  }
+
+  const templateVars = {id: req.params.id, longURL: urlData.longURL, userDetails: userData,};
   return res.render("urls_show", templateVars);
 });
 
+// Redirect route for short URLs
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id].longURL;
-  if(!longURL) {
-    return res.status(404).send("Short URL Cannot be found");
+  const link = urlDatabase[id];
+  if(!link) {
+    return res.status(404).render("error", { error: "Short URL not found" });
   }
+  const longURL = link.longURL;
   return res.redirect(longURL);
 });
 
+// Create a new URL route
 app.post("/urls", (req, res) => {
   const userDetails = users[req.session.user_id];
   if(!userDetails) {
-    return res.status(403).send("Only registered users can shorten URL");
-    return res.redirect("/login")
+    return res.status(403).render("error", { error: "Only registered users can shorten URL"});
   }
   let longURL = req.body.longURL;
   if(!longURL.startsWith("http://") && !longURL.startsWith("https://")) {
@@ -94,22 +115,23 @@ app.post("/urls", (req, res) => {
   if(!longURL.endsWith(".com")) {
     longURL = `${longURL}.com`;
   }
-  let shortURLID = generateRandomString(6);
+  let shortURLID = generateRandomString(6); // generates random strings as short URLs
   const userID = userDetails.id;
   let newdatabase = {
     longURL: longURL,
     userID: userID
   }
   urlDatabase[shortURLID] = newdatabase;
-  res.redirect("/urls");
+  res.redirect(`/urls/${shortURLID}`);
 });
 
+// Delete URL route
 app.post("/urls/:id/delete", (req, res) => {
   const userDetails = users[req.session.user_id];
   const userPages = urlsForUser(userDetails.id, urlDatabase);
   if(!userDetails || !userPages) {
-    return res.status(403).send("Only registered users can delete URLs");
-    return res.redirect("/login")
+    return res.status(403).render("error", { error: "Only registered users can delete URLs"});
+    
   }else{
     const deletItem = req.params.id;
     if(urlDatabase[deletItem]) {
@@ -123,12 +145,12 @@ app.post("/urls/:id/delete", (req, res) => {
   
 });
 
+// Update URL route
 app.post("/urls/:id/update", (req, res) => {
   const userDetails = users[req.session.user_id];
   const userPages = urlsForUser(userDetails.id, urlDatabase);
   if(!userDetails || !userPages) {
-    return res.status(403).send("Only registered users can update URL");
-    return res.redirect("/login")
+    return res.status(403).render("error", { error: "Only registered users can update URL"});
   } else{
     const updateItem = req.params.id;
     let editURL = req.body.longURL;
@@ -146,11 +168,12 @@ app.post("/urls/:id/update", (req, res) => {
   
 });
 
+// Edit URL route
 app.post("/urls/:id/edit", (req, res) => {
   const userDetails = users[req.session.user_id];
   const userPages = urlsForUser(userDetails.id, urlDatabase);
   if(!userDetails || !userPages) {
-    return res.status(403).send("Only registered users can edit URL");
+    return res.status(403).render("error", { error: "Only registered users can edit URL"});
     return res.redirect("/login")
   } else{
     const editItem = req.params.id;
@@ -164,6 +187,7 @@ app.post("/urls/:id/edit", (req, res) => {
   
 });
 
+// List of users URL route
 app.get("/urls", (req, res) => {
   const userDetails = users[req.session.user_id];
   if (userDetails) {
@@ -178,33 +202,38 @@ app.get("/urls", (req, res) => {
   }
 });
 
-
+// logout URL route
 app.post("/logout", (req, res) => {
-  req.session['user_id'] = null;
+  req.session = null;
   res.redirect("/login");
 });
 
+// Get  Registered Users route
 app.get("/register", (req, res) => {
   const userDetails = users[req.session.user_id];
+  if(userDetails) {
+    res.redirect("/urls");
+  }
   const templateVars = { 
     userDetails: userDetails,
   };
   res.render("register", templateVars);
 });
 
+// Register users route
 app.post("/register", (req, res) => {
   const email = req.body.email;
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
   const password = hashedPassword;
 
   if(!email || !password) {
-    return res.status(400).send("Please provide an email and a password");
+    return res.status(400).render("error", { error: "Please provide an email and a password"});
   }
 
   let foundUser = getUserByEmail(email, users);
   
   if (foundUser) {
-    return res.status(400).send("A User with this email already exists.");
+    return res.status(400).render("error", { error: "A User with this email already exists."});
   }
 
   const userRandomID = generateRandomString(4);
@@ -216,40 +245,44 @@ app.post("/register", (req, res) => {
 
   // Add a new user to the 'users' database
   users[userRandomID] = newUser;
-  console.log(newUser);
-  console.log(users);
-  res.redirect("/login");
+  req.session.user_id = userRandomID;
+  res.redirect("/urls");
+  
 });
 
+// Users Login route
 app.get("/login", (req, res) => {
   const userDetails = users[req.session.user_id];
+  if(userDetails) {
+    res.redirect("/urls");
+  }
   const templateVars = { 
     userDetails: userDetails,
   };
   res.render("login", templateVars );
 });
 
+// Users sign in route
 app.post("/login", (req, res) => {
   const userEmail = req.body.email;
   const userPword = req.body.password;
   
   if(!userEmail || !userPword) {
-    return res.status(400).send("Provide login details")
+    return res.status(400).render("error", { error: "Provide login details"})
   }
   
   let verifyEmail = getUserByEmail(userEmail, users);
     
   if(!verifyEmail) {
-    res.status(403).send("User not found");
+    res.status(403).render("error", { error: "User not found"});
   }
 
-  let verifyPword = bcrypt.compareSync(userPword, verifyEmail.password);
+  let verifyPword = bcrypt.compareSync(userPword, verifyEmail.password); //checking if user's password matches saved hashed password.
   
   if(!verifyPword) {
-    return res.status(403).send("Either email or password is invalid");
+    return res.status(403).render( "error", { error: "Either email or password is invalid"});
   }
-
-  req.session.user_id = verifyEmail.id;
+  req.session.user_id = verifyEmail.id; //setting user's ID cookies session
   res.redirect("/urls")
   
 });
